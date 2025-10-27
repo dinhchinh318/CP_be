810729cdfd3a1821128a6c75cae0060e74a2075e
dinhchinh318 <chinh3182005@gmail.com>
2025-10-27 10:50:45 +0700

    gg

--- src/app.js
+++ src/app.js
@@ -42,28 +42,33 @@ require("dotenv").config();
 const app = express();
 
 // CORS configuration
+// app.use(cors({
+//   origin: function(origin, callback) {
+//     const allowedOrigins = [
+//       'http://localhost:3000',
+//       'http://localhost:5173',
+//       process.env.FRONTEND_URL
+//     ].filter(Boolean);
+    
+//     // Cho phép requests không có origin (mobile apps, Postman, etc.)
+//     if (!origin) return callback(null, true);
+    
+//     if (allowedOrigins.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       console.log('Blocked by CORS:', origin);
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   credentials: true,
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization']
+// }));
 app.use(cors({
-  origin: function(origin, callback) {
-    const allowedOrigins = [
-      'http://localhost:3000',
-      'http://localhost:5173',
-      process.env.FRONTEND_URL
-    ].filter(Boolean);
-    
-    // Cho phép requests không có origin (mobile apps, Postman, etc.)
-    if (!origin) return callback(null, true);
-    
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      console.log('Blocked by CORS:', origin);
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: process.env.FRONTEND_URL,
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
 }));
+
 
 app.use(express.json());
 
